5d97530e6736db96273d2cbc773446b59d1ad554
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2019-10-17 23:04:23 -0700

    Initial commit with the base structures.

---
+++ QuestionnaireNotepad/Libraries/IAnswerType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireNotepad.Libraries
+{
+    interface IAnswerType
+    {
+        /// <summary>
+        /// Returns a plain-text friendly value for this object.
+        /// </summary>
+        /// <returns>A string that represents the content of the object</returns>
+        string GetFlatText();
+        /// <summary>
+        /// Gets the number of answers this object contains
+        /// </summary>
+        /// <returns>Integer that represents an index</returns>
+        int GetTotalChoices();
+        /// <summary>
+        /// Returns the type of this answer.
+        /// </summary>
+        /// <returns>Returns an value defined by the enumeration AnswerTypes</returns>
+        AnswerTypes GetType();
+    }
+}
