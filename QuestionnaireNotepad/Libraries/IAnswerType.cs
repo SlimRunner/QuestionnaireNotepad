30ef5f3019adfff0ef680b5cb995fad974513fe2
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2019-10-18 22:35:46 -0700

    Added aggregator class

--- QuestionnaireNotepad/Libraries/IAnswerType.cs
+++ QuestionnaireNotepad/Libraries/IAnswerType.cs
@@ -13,7 +13,7 @@ namespace QuestionnaireNotepad.Libraries
     interface IAnswerType
     {
         /// <summary>
-        /// Returns a plain-text friendly value for this object.
+        /// Returns a formatted plain-text value for this object.
         /// </summary>
         /// <returns>A string that represents the content of the object</returns>
         string GetFlatText();
--- QuestionnaireNotepad/Libraries/QuestionItem.cs
+++ QuestionnaireNotepad/Libraries/QuestionItem.cs
@@ -8,6 +8,41 @@ namespace QuestionnaireNotepad.Libraries
 {
     class QuestionItem
     {
+        public QuestionItem(string question, IAnswerType answer)
+        {
+            Question = question ?? throw new ArgumentNullException(nameof(question));
+            Answer = answer ?? throw new ArgumentNullException(nameof(answer));
+        }
 
+        public string Question
+        {
+            get
+            {
+                return Question;
+            }
+
+            set
+            {
+                Question = value;
+            }
+        }
+
+        public IAnswerType Answer
+        {
+            get
+            {
+                return Answer;
+            }
+
+            set
+            {
+                Answer = value;
+            }
+        }
+
+        public string GetFieldText()
+        {
+            return Answer.GetType().ToString() + "$;" + Question + "$;" + Answer.GetFlatText();
+        }
     }
 }
