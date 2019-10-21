db757d872d43ae1aac438a8d8ae83ec37b88a63a
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2019-10-20 23:14:48 -0700

    Added tuple lists for regex replacements.

--- QuestionnaireNotepad/DataParsing/RecordDispatcher.cs
+++ QuestionnaireNotepad/DataParsing/RecordDispatcher.cs
@@ -9,47 +9,37 @@ namespace QuestionnaireNotepad.DataParsing
 {
     class RecordDispatcher
     {
+        static List<(Regex, string)> SanitizeRxSeq = new List<(Regex regex, string replacement)>()
+        {
+            (new Regex(@"([\\\;\:\,])"), @"\$1"),
+            (new Regex(@"\r\n?|\n"), @"\n")
+        };
+
+        static List<(Regex, string)> ParseRxSeq = new List<(Regex regex, string replacement)>()
+        {
+            (new Regex(@"(?<!\\)\\n"), System.Environment.NewLine),
+            (new Regex(@"(\\)(?=[\;\:\,])"), ""),
+            (new Regex(@"(\\{2})"), @"\")
+        };
+
         static public string SanitizeString(string param)
         {
-
-            /*
-            IDictionary<string, string> SanitizeSequence = new Dictionary<string, string>
-            {
-                { @"([\\\;\:\,])", @"\\$1" },
-                { @"\n", @"\\n" }
-            };
-            */
-
             string retval = param;
-            Regex SanitizePattern = new Regex(@"([\\\;\:\,])");
-            retval = SanitizePattern.Replace(retval, @"\$1");
-
-            SanitizePattern = new Regex(@"\r\n?|\n");
-            retval = SanitizePattern.Replace(retval, @"\n");
+            foreach (var (regex, replacement) in SanitizeRxSeq)
+            {
+                retval = regex.Replace(retval, replacement);
+            }
 
             return retval;
         }
 
         static public string ParseRecord(string param)
         {
-
-            /*
-            IDictionary<string, string> SanitizeSequence = new Dictionary<string, string>
-            {
-                { @"([\\\;\:\,])", @"\\$1" },
-                { @"\n", @"\\n" }
-            };
-            */
-
             string retval = param;
-            Regex SanitizePattern = new Regex(@"(?<!\\)\\n");
-            retval = SanitizePattern.Replace(retval, System.Environment.NewLine);
-
-            SanitizePattern = new Regex(@"(\\)(?=[\;\:\,])");
-            retval = SanitizePattern.Replace(retval, "");
-
-            SanitizePattern = new Regex(@"(\\{2})");
-            retval = SanitizePattern.Replace(retval, @"\");
+            foreach (var (regex, replacement) in ParseRxSeq)
+            {
+                retval = regex.Replace(retval, replacement);
+            }
 
             return retval;
         }
